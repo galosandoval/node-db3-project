2b423b184e5af9ac2f378950334ae327e5c269a5
Galo Sandoval <galosan92@gmail.com>
2020-09-09 16:36:05 -0700

    finished 3 models

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -1,1 +1,22 @@
-const Db = require('../data/')
+const db = require('../data/db-config')
+
+module.exports = {
+  find,
+  findById,
+  findSteps,
+  // add,
+  // update,
+  // remove
+}
+
+function find() {
+  return db('schemes')
+}
+
+function findById(id) {
+  return db('schemes').where({id})
+}
+
+function findSteps(id) {
+  return db('steps').where({scheme_id: id}).join('schemes', 'schemes.id', 'steps.scheme_id')
+}
