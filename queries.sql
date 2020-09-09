ba22aa97deeb041c1535fac58ea298e4c9cbc7af
Galo Sandoval <galosan92@gmail.com>
2020-09-09 16:01:43 -0700

    finished queries

--- queries.sql
+++ queries.sql
@@ -14,5 +14,13 @@ WHERE [order].OrderDate
   BETWEEN [2012-07-04] AND [2012-08-09]
 ORDER BY [order].OrderDate
 -- Display the name and quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.
-
+SELECT product.ProductName, OrderDetail.Quantity
+FROM OrderDetail
+JOIN Product 
+  ON OrderDetail.ProductId = Product.Id
+WHERE OrderDetail.OrderId = 10252
 -- Display the OrderID, Customer's Company Name and the employee's LastName for every order. All columns should be labeled clearly. Displays 16,789 records.
+SELECT [Order].Id, Customer.CompanyName AS Customer, Employee.LastName AS Contact
+FROM [Order]
+JOIN Customer ON Customer.Id = [Order].CustomerId
+JOIN Employee ON Employee.Id = [Order].EmployeeId
